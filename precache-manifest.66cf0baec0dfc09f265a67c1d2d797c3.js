af4063de74c09b73732ee7dac434aac0efc3af5d
boostup <boostup@gmail.com>
2020-08-09 07:27:16 +0200

    Updates

---
+++ precache-manifest.66cf0baec0dfc09f265a67c1d2d797c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1224e5eba47e1faca2de04a40b2feca",
+    "url": "/react-hooks-mui-todo-app/index.html"
+  },
+  {
+    "revision": "590e31da688d013f9db8",
+    "url": "/react-hooks-mui-todo-app/static/css/main.449e4a77.chunk.css"
+  },
+  {
+    "revision": "00d2d8548a6d117dd0af",
+    "url": "/react-hooks-mui-todo-app/static/js/2.5f4f1b61.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-hooks-mui-todo-app/static/js/2.5f4f1b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590e31da688d013f9db8",
+    "url": "/react-hooks-mui-todo-app/static/js/main.a95b3ef3.chunk.js"
+  },
+  {
+    "revision": "5d0c61ae91cce33ea729",
+    "url": "/react-hooks-mui-todo-app/static/js/runtime-main.1d2fa0d5.js"
+  }
+]);
